0dedb630d83e629b255956adeb9396cd4b1ce631
mokopo <naka.mokopo@gmail.com>
2024-08-09 00:39:37 +0900

    Updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,28 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
-
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-);
-
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
+ 
+const IndexPage: NextPage = () => {
+    // ❶ useStateを使って状態を定義する
+    const [imageUrl, setImageUrl] = useState("");
+    const [loading, setLoading] = useState(true);
+    // ❷ マウント時に画像を読み込む宣言
+    useEffect(() => {
+        fetchImage().then((newImage) => {
+            setImageUrl(newImage.url); // 画像URLの状態を更新する
+            setLoading(false); // ローディング状態を更新する
+        });
+    }, []);
+    // ❸ ローディング中でなければ、画像を表示する
+    return <div>{loading || <img src={imageUrl} />}</div>;
+}
 export default IndexPage;
+
+type Image = {
+    url: string;
+};
+const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    console.log(images);
+    return images[0];
+};

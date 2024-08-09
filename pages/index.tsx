50d4faac287bf30fff393bb9456abae924a91021
mokopo <naka.mokopo@gmail.com>
2024-08-10 00:11:19 +0900

    Updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,14 +29,11 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
     return (
         <div className={styles.page}>
             <button onClick={handleClick} className={styles.button}>
-                他のにゃんこも見る
+            One more cat!
             </button>
             <div className={styles.frame}>
                 {loading || <img src={imageUrl} className={styles.img} />}
             </div>
-            <button onClick={handleClick} className={styles.button}>
-                他のにゃんこも見る
-            </button>
         </div>
     );
 }
